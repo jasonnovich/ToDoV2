326e6f2cfde53c4133f039aaf1957ff5e84cf0ac
Jason Novich <jasonnovich@users.noreply.github.com>
2018-07-26 19:47:53 +0300

    database setup with javadocs documentation

--- app/src/main/java/o2pm/com/todov2/Data/DatabaseHandler.java
+++ app/src/main/java/o2pm/com/todov2/Data/DatabaseHandler.java
@@ -116,9 +116,5 @@ public class DatabaseHandler extends SQLiteOpenHelper {
     public int getToDoListItemCount() {
         return 0;
     }
-
-    /*Make sure that when you create the database you are creating it with one entry which is the list name.
-When adding the actual to do list item, that would be an update o1f the database.
-Each change in the actual to do list item will mean an update of the item as well.
- */
+    
 }
