0c293a71d1af7fa040f5adadf640e401d242b51b
Jason Novich <jasonnovich@users.noreply.github.com>
2018-08-01 08:44:32 +0300

    datahandler methods creation

--- app/src/main/java/o2pm/com/todov2/util/Constants.java
+++ app/src/main/java/o2pm/com/todov2/util/Constants.java
@@ -17,13 +17,14 @@ public class Constants {
     public static final String ACTUALDETAILS = "actual_details";        //User input - actual details that the user enters for the to do item
     public static final int ACTUALPERCENTAGE = 0;           //User input - actual percentage complete in #
 
-    //Static items in the UI
-//    public static final String DUEON = null;
-//    public static final String LOCATION = null;
-//    public static final String ASSIGNEDTO = null;
-//    public static final String TODODETAILS = null;
-//    public static final String CATEGORY = null;
-//    public static final String PERCENTAGECOMPLETE = null;
+/*    Static items in the UI
+    public static final String DUEON = null;
+    public static final String LOCATION = null;
+    public static final String ASSIGNEDTO = null;
+    public static final String TODODETAILS = null;
+    public static final String CATEGORY = null;
+    public static final String PERCENTAGECOMPLETE = null;
+*/
 
 
 
